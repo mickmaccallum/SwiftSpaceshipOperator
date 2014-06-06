0d3223620ad275501bc856542e19900388071f32
Michael MacCallum <maccdevteam@gmail.com>
2014-06-06 13:50:15 -0400

    A little more generic

--- Spaceship Operator/SpaceshipOperator.swift
+++ Spaceship Operator/SpaceshipOperator.swift
@@ -6,15 +6,21 @@
 //  Copyright (c) 2014 Michael MacCallum. All rights reserved.
 //
 
+enum SpaceshipValue: Int8 { // Explicitly typed to allow toRaw()
+    case less = -1
+    case greater = 1
+    case same = 0
+}
+
 operator infix <=> { associativity left precedence 140 }
 
-@infix func <=> (left: Double, right: Double) -> Int {
+@infix func <=> <T: Comparable> (left: T, right: T) -> SpaceshipValue {
 
     if left < right {
-        return -1
+        return .less
     } else if left > right {
-        return 1
+        return .greater
     } else {
-        return 0
+        return .same
     }
 }
